cbcb2b277f97b0ebe5ff75bce3a5eabb5c9b1068
Thiago Neves <thiagoantonio.neves@usp.br>
2025-08-15 16:22:17 -0300

    Finishing the leaky bucket middleware

--- src/index.ts
+++ src/index.ts
@@ -31,6 +31,9 @@ const app = new Koa();
 const router = new KoaRouter();
 const PORT = 3000;
 
+const buckets = new Map<number, IClientBucket>();
+const CAPACITY = 10;
+const DRIP_INTERVAL = 1000 * 60 * 60; // 1 hour = 3600000 ms
 
 app.use(koaBodyParser());
 
@@ -74,7 +77,38 @@ router.get('/me', async (ctx) => {
     }
 });
 
-//################################################## APP ##################################################
+//################################################## MIDDLEWARES ##################################################
+
+// Leaky bucket middleware
+app.use(async (ctx, next) => {
+    
+  const userId = ctx.state.user.id;
+
+
+  if (!buckets.has(userId)) {
+    buckets.set(userId, { tokens: CAPACITY, lastDrip: Date.now() });
+  }
+
+  const bucket = buckets.get(userId)!;
+
+  // Checks for new tokens available
+  const now = Date.now();
+  const timePassed = now - bucket.lastDrip;
+  const tokensToAdd = Math.floor(timePassed / DRIP_INTERVAL);
+
+  bucket.tokens = Math.min(bucket.tokens + tokensToAdd, CAPACITY);
+  bucket.lastDrip = now;
+
+  if (bucket.tokens > 0) {
+    bucket.tokens--;
+    await next();
+  } else {
+    ctx.status = 429; // Too Many Requests
+    ctx.body = { error: 'Request limit reached' };
+  }
+});
+
+
 app.use(async (ctx, next) => {
 
     const authHeader = ctx.request.headers.authorization;
