0e51dab0cc7bf53f2cf278fc3693976d052d1d91
Thiago Neves <thiagoantonio.neves@usp.br>
2025-08-21 21:39:28 -0300

    Start of config

---
+++ relay.config.js
@@ -0,0 +1,7 @@
+/** @type {import('relay-compiler').Config} */
+module.exports = {
+  src: "./src",
+  schema: "./schema.graphql",
+  language: "typescript",
+  artifactDirectory: "./src/__generated__",
+};
