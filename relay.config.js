920746517135eda899886dbf85583dd1299113b4
Thiago Neves <thiagoantonio.neves@usp.br>
2025-08-21 21:43:11 -0300

    Server changed to dedicated folder

--- relay.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('relay-compiler').Config} */
-module.exports = {
-  src: "./src",
-  schema: "./schema.graphql",
-  language: "typescript",
-  artifactDirectory: "./src/__generated__",
-};
